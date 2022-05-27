611a6473a3d1d4e8639ca2070ac189ba7c52f1f3
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2022-05-27 20:15:20 +0530

    Update blacklist.py

--- scenario/modules/blacklist.py
+++ scenario/modules/blacklist.py
@@ -440,7 +440,7 @@ def __stats__():
         sql.num_blacklist_filters(), sql.num_blacklist_filter_chats())
 
 
-__mod_name__ = "Blacklists"
+__mod_name__ = "ʙʟᴀᴄᴋʟɪsᴛs🙅"
 
 __help__ = """
 Blacklists are used to stop certain triggers from being said in a group. Any time the trigger is mentioned, the message will immediately be deleted. A good combo is sometimes to pair this up with warn filters!
